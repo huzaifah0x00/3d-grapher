63c9682f44f621070302220688152bc19f8b97d3
huzaifah0x00 <huzaifashaikh0012@gmail.com>
2020-02-19 17:23:05 +0500

    commit before moving to nodejs

--- grapher/main_app/views.py
+++ grapher/main_app/views.py
@@ -9,8 +9,15 @@ def make_graph(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        equations = [eq for eq in request.POST if eq.startswith('equation')]
-        return HttpResponse([eq for eq in equations])
+        equations = dict()
+        for eq in request.POST:
+            if not eq.startswith('equation'):  # skip irrelevent parameters
+                continue
+            else:
+                equations[eq] = request.POST[eq]
+
+
+        return render(request, 'main_app/graph.html', context={'equations':equations})
 
     else:
         return HttpResponse("404, <a href='/'>home</a>")
